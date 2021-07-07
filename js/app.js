dab7c4e14036737a61e3a01cafb44daa30a47de1
HenryJrA <henryjrallam7@gmail.com>
2021-07-06 21:22:06 -0400

    edited

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ let maxNum = 100;
 let range = maxNum - miniNum;
 let n = [];
 let letter;
-let gameOver = true;
+// let gameOver = true;
 let table = document.querySelector("table")
 table.onclick = function(e){
     console.log(e);
@@ -33,19 +33,46 @@ var winSets = [
 
 function init(){
 for(i=0; i<=24; i++){
+    // query seletor by element or class and ten
     let rdmnm= Math.floor(Math.random() * range) + miniNum;
-    if (rdmnm >= 1 && rdmnm <= 20) letter ="B"    
+    let B = Math.floor(Math.random() * 24) + 1
+    let B1 = Math.floor(Math.random() * 23) + 1
+    let B2 = Math.floor(Math.random() * 22) + 1
+    let B3 = Math.floor(Math.random() * 21) + 1
+    let B4 = Math.floor(Math.random() * 20) + 1
+    
+    // if (rdmnm >= 1 && rdmnm <= 20) letter ="B"    
     if (rdmnm >= 21 && rdmnm <= 40) letter ="I"
     if (rdmnm >= 41 && rdmnm <= 60) letter ="N"
     if (rdmnm >= 61 && rdmnm <= 80) letter ="G"
     if (rdmnm >= 81 && rdmnm <= 100) letter ="O"
-    if(n.indexOf(rdmnm) < 0) rdmnm.push(n);
+    // if(n.indexOf(rdmnm) < 0) rdmnm.push(n);
     
     console.log(letter + rdmnm);
-    
-    
+    document.getElementById("square0").innerHTML=B 
+    document.getElementById("square1").innerHTML=B1 
+    document.getElementById("square2").innerHTML=B2 
+    document.getElementById("square3").innerHTML=B3 
+    document.getElementById("square4").innerHTML=B4 
+    document.getElementById("square4").innerHTML=I 
+    document.getElementById("square4").innerHTML=I 
+    document.getElementById("square4").innerHTML=I
+    document.getElementById("square4").innerHTML=I
+    document.getElementById("square4").innerHTML=I
+    document.getElementById("square4").innerHTML=N 
+    document.getElementById("square4").innerHTML=N 
+    document.getElementById("square4").innerHTML=N 
+    document.getElementById("square4").innerHTML=N 
+    document.getElementById("square4").innerHTML=N 
+    document.getElementById("square4").innerHTML=G
+    document.getElementById("square4").innerHTML=G
+    document.getElementById("square4").innerHTML=G 
+    document.getElementById("square4").innerHTML=G
+    document.getElementById("square4").innerHTML=G
+
 }
 }
+
 init()
 function checkBingo(){
     winSets.forEach(checkwinning)
@@ -68,31 +95,31 @@ function clickedCell(){
 
     }
 }
-function createBoard() {
+// function createBoard() {
     
-    // get a reference to the table element that contains the board
-    var board = document.getElementById("tblsq");
+//     // get a reference to the table element that contains the board
+//     var board = document.getElementById("tblsq");
     
-    // create the rows and columns that make up the board
-    for (row = 0; row < NUM_ROWS; row++) {
+//     // create the rows and columns that make up the board
+//     for (row = 0; row < NUM_ROWS; row++) {
         
-        var tr = document.createElement("tr");
-        for (col = 0; col < NUM_COLS; col++) {
+//         var tr = document.createElement("tr");
+//         for (col = 0; col < NUM_COLS; col++) {
             
-            var td = document.createElement("td");
+//             var td = document.createElement("td");
             
-            // by default, all cells are unselected
-            td.className = "empty";
-            tr.appendChild(td); 
+//             // by default, all cells are unselected
+//             td.className = "empty";
+//             tr.appendChild(td); 
             
-            // make all cells clickable, except FREE space
-            if (row !== 2 || col !== 2) {
-                td.addEventListener("click", clickCell);
-            }
-        }
-        board.appendChild(tr);
-    }    
-}
+//             // make all cells clickable, except FREE space
+//             if (row !== 2 || col !== 2) {
+//                 td.addEventListener("click", clickCell);
+//             }
+//         }
+//         board.appendChild(tr);
+//     }    
+// }
 // const squares = document.getElementById("square");
 // squares.forEach((square)=>{
 //     square.addEventListener("click","handleClick")
