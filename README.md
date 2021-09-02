56d61628d58f9394bea83911f6e580c2c34c846d
HenryJrA <henryjrallam7@gmail.com>
2021-09-02 18:33:15 -0400

    adjustments

--- README.md
+++ README.md
@@ -1,32 +1,31 @@
- bingo-project
+ ## bingo-project
 
-##objective:
+## Objective:
 -create a browser-based game usig HTML, CSS, JS
 
-MOTIVATION:
+## MOTIVATION:
 During highscool bingo was my favorite game to play when we had time in class or some free time to do so, my friends and i used to play it a lot and i decided why not to make one.
 
 to play the game:
-click [here](https://bingo-project1.surge.sh)./
+click [here](https://bingo-project1.surge.sh).
 
 Here is my initial wire-frame of how the game should looks like:
-WireFrame
- (https://imgur.com/9nW8mDi)
-  Here is how my game looks like:
- (https://imgur.com/9nW8mDi)
- 
- 
- Technology used :
+## WireFrame:
+<img src ='images/wireframe.png'>
+
+## Here is how my game looks like:
+<img src='images/bingoresume.png'>
+ ## Technology used :
  HTML
  CSS
  JavaScript
  Surge
  GIT
+ 
 
 
-pseudocode:
 
-BINGO
+## BINGO
 1. GAME START BY A 5X5 GRID WITH NUMBER IN EACH CELL FROM 1 TO 100  RANDOMLY PLACED 
 2. BIG BALL CONTAINING ALL THE BALLS FOR EACH NUMBER  
   .1 PICK A NUMBER RANDOMLY
@@ -39,16 +38,16 @@ ANNOUCE BINGO
 4. WHOEVER FINISH A ROW OR COLUMN FIRST AND ANNOUCE BINGO FIRST WINS
 
 ## Minimum Viable Product:
--As a user, i should be able to refresh the poage and new numbers will show up on the screen 
--as a user i should be able to see the  numbers on the screen  
--As a user i should be able to press on a number in the table and highlght thew umber chosen
--As a user if winning conditons are met, it's a BINGO!
--reset top refresh teh page 
+- As a user, i should be able to refresh the poage and new numbers will show up on the screen 
+- as a user i should be able to see the  numbers on the screen  
+- As a user i should be able to press on a number in the table and highlght thew umber chosen
+- As a user if winning conditons are met, it's a BINGO!
+- reset top refresh the page 
 ## Stretch Goals:
--add a ball that display all the number at a time 
--add an accurate winning condition, if wrong numbers pressed display "BAD BINGO"
--add a play/pause button and a button to press whenever the user wants the numbers to be shown
--fix the CSS and more styling to it with sounds and images
--fix my code so it can work properly
--add mobile responsive 
-and add a computer player to compete with the user
+- [x] add a ball that display all the number at a time 
+- [ ] add an accurate winning condition, if wrong numbers pressed display "BAD BINGO"
+- [ ] add a play/pause button and a button to press whenever the user wants the numbers to be shown
+- [ ] fix the CSS and more styling to it with sounds and images
+- [x] fix my code so it can work properly
+- [ ] add mobile responsive 
+- [ ] add a computer player to compete with the user
--- js/app.js
+++ js/app.js
@@ -1,105 +1,99 @@
+window.onload = initAll;
+var usedNums = new Array(76);
 
-let  miniNum = 1;
-let maxNum = 100;
-let range = maxNum - miniNum;
-let n = [];
-let letter;
-let winner;
-let player;
-
-var winSets = [
-    [0, 1, 2, 3, 4],
-    [5, 6, 7, 8, 9],
-    [10, 11, 12, 13, 14],
-    [15, 16, 17, 18, 19],
-    [20, 21, 22, 23, 24],
-    [0, 5, 10, 15, 20],
-    [1, 6, 11, 16, 21],
-    [2, 7, 12, 17, 22],
-    [3, 8, 13, 18, 23],
-    [4, 9, 14, 19, 24],
-    [0, 6, 12, 18, 24],
-    [4, 8, 12, 16, 20]
-];
-
-
-let tables = document.querySelector("table")
 const currentBallDiv = document.querySelector("#current-ball");
-const newNbr = document.getElementById("new-number");
-
-tables.onclick = function(e){
-    console.log(e);
-    highlight(e.target);
-    
-}
-function highlight(div){
-    div.classList.toggle("highligh");
-    div.style.color = "black"
+function initAll() {
+	if (document.getElementById) {
+		document.getElementById("reload").onclick = anotherCard;
+		newCard();
+	}
+	else {
+		alert("Sorry, your browser doesn't support this script");
+	}
 }
 
-init()
-function init(){
 
-    let B = Math.floor(Math.random() *  (20) + 1)
-    let B1 = Math.floor(Math.random() * (20) + 1)
-    let B2 = Math.floor(Math.random() * (20) + 1)
-    let B3 = Math.floor(Math.random() * (20) + 1)
-    let B4 = Math.floor(Math.random() * (20) + 1)
-    let I = Math.floor(Math.random() *  (40-20+1) + 21);
-    let I1 = Math.floor(Math.random() * (40-20+1) + 21);
-    let I2 = Math.floor(Math.random() * (40-20+1) + 21);
-    let I3 = Math.floor(Math.random() * (40-20+1) + 21);
-    let I4 = Math.floor(Math.random() * (40-20+1) + 21);
-    let N = Math.floor(Math.random() *  (60-41+1) + 41);
-    let N1 = Math.floor(Math.random() * (60-41+1) + 41);
-    let N2 = Math.floor(Math.random() * (60-41+1) + 41);
-    let N3 = Math.floor(Math.random() * (60-41+1) + 41);
-    let N4 = Math.floor(Math.random() * (60-41+1) + 41);
-    let G = Math.floor(Math.random() *  (80-61+1) + 61);
-    let G1 = Math.floor(Math.random() * (80-61+1) + 61);
-    let G2 = Math.floor(Math.random() * (80-61+1) + 61);
-    let G3 = Math.floor(Math.random() * (80-61+1) + 61);
-    let G4 = Math.floor(Math.random() * (80-61+1) + 61);
-    let O = Math.floor(Math.random() *  (100-81+1) + 81);
-    let O1 = Math.floor(Math.random() * (100-81+1) + 81);
-    let O2 = Math.floor(Math.random() * (100-81+1) + 81);
-    let O3 = Math.floor(Math.random() * (100-81+1) + 81);
-    let O4 = Math.floor(Math.random() * (100-81+1) + 81);
-    // if(n.indexOf(rdmnm) < 0) rdmnm.push(n);
-    
-    document.getElementById("square0").innerHTML=B 
-    document.getElementById("square1").innerHTML=B1 
-    document.getElementById("square2").innerHTML=B2 
-    document.getElementById("square3").innerHTML=B3 
-    document.getElementById("square4").innerHTML=B4 
-    document.getElementById("square5").innerHTML=I 
-    document.getElementById("square6").innerHTML=I1 
-    document.getElementById("square7").innerHTML=I2
-    document.getElementById("square8").innerHTML=I3
-    document.getElementById("square9").innerHTML=I4
-    document.getElementById("square10").innerHTML=N 
-    document.getElementById("square11").innerHTML=N1 
-    document.getElementById("square12").innerHTML=N2 
-    document.getElementById("square13").innerHTML=N3 
-    document.getElementById("square14").innerHTML=N4 
-    document.getElementById("square15").innerHTML=G
-    document.getElementById("square16").innerHTML=G1
-    document.getElementById("square17").innerHTML=G2 
-    document.getElementById("square18").innerHTML=G3
-    document.getElementById("square19").innerHTML=G4
-    document.getElementById("square20").innerHTML=O
-    document.getElementById("square21").innerHTML=O1
-    document.getElementById("square22").innerHTML=O2
-    document.getElementById("square23").innerHTML=O3
-    document.getElementById("square24").innerHTML=O4
-    
+function newCard() {
+	for (var i=0; i<24; i++) {
+		setSquare(i);
+	}
+}
 
-    player =1
-    
-    winner =null
-    
-}   
 
+function setSquare(thisSquare) {
+	var currSquare = "square" + thisSquare;
+	var colPlace = new Array(0,0,0,0,0,1,1,1,1,1,2,2,2,2,3,3,3,3,3,4,4,4,4,4);
+	var colBasis = colPlace[thisSquare] * 15;
+	var newNum;
+
+	do {
+		newNum = colBasis + getNewNum() + 1;
+	}
+	while (usedNums[newNum]);
+    usedNums[newNum] = true;
+    document.getElementById(currSquare).innerHTML = newNum;
+    document.getElementById(currSquare).className = "";
+    document.getElementById(currSquare).onmousedown = toggleColor;
+}
+
+
+function getNewNum() {
+	return Math.floor(Math.random() * 15);
+}
+
+
+function anotherCard() {
+	for (var i=1; i<usedNums.length; i++) {
+		usedNums[i] = false;
+	}
+	newCard();
+	return false;
+}
+
+
+function toggleColor(evt) {
+	if (evt) {
+		var thisSquare = evt.target;
+	}	else {
+		var thisSquare = window.event.srcElement;
+	}
+	if (thisSquare.className == "") {
+		thisSquare.className = "pickedBG";
+	}	else {
+		thisSquare.className = "";
+	}
+	checkWin();
+}
+
+
+function checkWin() {
+	var winningOption = -1;
+	var setSquares = 0;
+	var winners = new Array(31,992,15360,507904,541729,557328,1083458,2162820,4329736,8519745,8659472,16252928);
+
+	for (var i=0; i<24; i++) {
+		var currSquare = "square" + i;
+		if (document.getElementById(currSquare).className != "") {
+			document.getElementById(currSquare).className = "pickedBG";
+			setSquares = setSquares | Math.pow(2,i);
+		}
+	}
+
+	for (var i=0; i<winners.length; i++) {
+		if ((winners[i] & setSquares) == winners[i]) {
+			winningOption = i;
+		}
+	}
+	
+	if (winningOption > -1) {
+		for (var i=0; i<24; i++) {
+			if (winners[winningOption] & Math.pow(2,i)) {
+				currSquare = "square" + i;
+				document.getElementById(currSquare).className = "winningBG";
+			}
+		}
+	}
+};
 
 
 
@@ -136,34 +130,3 @@ numDisplayIntervalId = setInterval(() => {
 }, 5000);
 
 
-
-function getWinner(){
-    
-    if (Math.abs(table[0] + table[1] + table[2] + table[3] + table[4]) === 5) return table[0];
-    if (Math.abs(table[5] + table[6] + table[7] + table[8] + table[9]) === 5) return table[5];
-    if (Math.abs(table[10] + table[11] + table[12] + table[13] + table[14]) === 5) return table[10];
-    if (Math.abs(table[15] + table[16] + table[17] + table[18] + table[19]) === 5) return table[15];
-    if (Math.abs(table[20] + table[21] + table[22] + table[23] + table[24]) === 5) return table[20];
-    if (Math.abs(table[0] + table[5] + table[10] + table[15] + table[20]) === 5) return table[0];
-    if (Math.abs(table[1] + table[6] + table[11] + table[16] + table[21]) === 5) return table[1];
-    if (Math.abs(table[2] + table[7] + table[12] + table[13] + table[14]) === 5) return table[2];
-    if (Math.abs(table[3] + table[8] + table[13] + table[18] + table[23]) === 5) return table[3];
-    if (Math.abs(table[4] + table[9] + table[14] + table[19] + table[24]) === 5) return table[4];
-    if (Math.abs(table[0] + table[6] + table[12] + table[18] + table[24]) === 5) return table[0];
-    if (Math.abs(table[4] + table[8] + table[12] + table[16] + table[20]) === 5) return table[4];
-    if(highlight =true){
-        return msg.innerHTML = "BINGO"
-    }else{
-        
-        return msg.innerHTML ="BINGO!"
-    }
-}
-
-let bingoBtn =document.getElementById("bingo").addEventListener("click", function(){
-    window.alert("BINGO!!")})
-           
-document.getElementById("reset")
-function reset(){
-    reset = location.reload()
-    reset.addEventListener("click", reload, false)
-}
